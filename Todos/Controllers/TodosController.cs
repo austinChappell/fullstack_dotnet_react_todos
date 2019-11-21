4e4a38d8f65f9c9b280096183d44e550296ddc08
Austin <austin.w.chappell@gmail.com>
2019-11-20 23:21:35 -0600

    first commit

---
+++ Todos/Controllers/TodosController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Todos.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class TodosController : ControllerBase
+  {
+    private static List<TodoItem> Todos = new List<TodoItem> { };
+
+    [HttpGet]
+    public ActionResult<List<TodoItem>> Get()
+    {
+      return Ok(Todos);
+    }
+
+    [HttpPost]
+    public ActionResult Post(TodoItem todoItem)
+    {
+      var existingItem = Todos.Find(item =>
+        item.Task.Equals(todoItem.Task, StringComparison.InvariantCultureIgnoreCase));
+
+      if (existingItem != null)
+      {
+        return BadRequest("Todo already exists.");
+      }
+      else
+      {
+        Todos.Add(todoItem);
+        var resourceUrl = Path.Combine(Request.Path.ToString(), Uri.EscapeUriString(todoItem.Task));
+        return Created(resourceUrl, todoItem);
+      }
+    }
+
+    [HttpPut]
+    public ActionResult Put(TodoItem todoItem)
+    {
+      var existingItem = Todos.Find(item =>
+        item.Task.Equals(todoItem.Task, StringComparison.InvariantCultureIgnoreCase));
+
+      if (existingItem == null)
+      {
+        return BadRequest("Todo not found.");
+      }
+      else
+      {
+        existingItem.Done = todoItem.Done;
+        return Ok(existingItem);
+      }
+    }
+
+    [HttpDelete]
+    [Route("{task}")]
+    public ActionResult Delete(string task)
+    {
+      var existingItem = Todos.Find(item =>
+        item.Task.Equals(task, StringComparison.InvariantCultureIgnoreCase));
+
+      if (existingItem == null)
+      {
+        return NotFound();
+      }
+
+      Todos.Remove(existingItem);
+      return NoContent();
+    }
+  }
+}
---
+++ Todos/Todos.cs
@@ -0,0 +1,8 @@
+namespace Todos
+{
+  public class TodoItem
+  {
+    public string Task { get; set; }
+    public bool Done { get; set; }
+  }
+}
